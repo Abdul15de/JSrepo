10ccba65fab8c7b3e5ab4d9bfa9cae2ce666ca4e
Abdul15de <126737630+Abdul15de@users.noreply.github.com>
2024-07-20 10:22:55 +0000

    basics of data types and variables

---
+++ JS_01/dataTypes.js
@@ -0,0 +1,45 @@
+// datatype is used to handle different types of data
+
+//7 tyoes of datatypes
+a = 25;
+
+/* ******* Tsting the type of some random values
+
+// console.log(typeof(a)); // number
+// console.log(typeof("abdull"));//string
+
+// console.log(typeof(0.5));//number
+// console.log(typeof("0.6"));//string
+// console.log(typeof('a'));//string
+// console.log(typeof("a"));//string
+// console.log(typeof('ass'));//string
+// console.log(typeof(null));//object**interview question
+// console.log(typeof(true));//boolean
+// console.log(typeof(abdul));// undefined
+   
+// console.log(typeof(yes));// undefined
+// console.log(typeof(dfg));// undefined
+
+*/
+
+/*  ***** NUMBERS *******/
+// ANY +VE OR -VE Or any floating point number(decimal number) comes under number data type 
+
+
+
+//    **** NaN- Not a Number *******
+
+
+
+/*
+console.log(0/0); // NaN
+console.log(typeof(NaN)); //number
+// ********** so NaN is a typeof number that represents a nvalid number e.g 0/0 is infinte so it represents a invalid number
+//  similarly ioperations on NaN ia also NaN
+// like --
+// 5*NaN--NaN
+// 5+NaN -- NaN
+// 5/NaN -- NaN 
+
+
+*/
---
+++ JS_01/variables.js
@@ -0,0 +1,7 @@
+/* varible is the name of a storage loation*/
+
+// variable values can be changed
+
+// declaring variables
+a=10;
+name = "Abdullah"; //name(string) is stored in "" double braces
