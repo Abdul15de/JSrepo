fdc77e80950a15728b292fa7a1a9d1084264aaa2
Abdul15de <126737630+Abdul15de@users.noreply.github.com>
2024-07-24 14:36:59 +0000

    alert,prompt,consol.error and console.warn

---
+++ JS_01/alertAndPrompt.js
@@ -0,0 +1,18 @@
+//  ***** ALERT IS USED TO DISPLAY ALERT MESSAGES *****
+
+// alert("something is wrong");
+
+
+// ***  PROMPT ********
+
+prompt("enter a number");
+
+//***note-> prompt and alert are not shown here it is shown in browser */
+
+// **console.error() and console.warn
+
+/*
+console.error("this is an error line"); // here it comes in red line in browser it comes as error usually comes
+
+console.warn("this is an warnng"); // here it comes in yellow line but in browser it comes as warning usuallyy comes.
+*/
